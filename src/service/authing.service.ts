0f4d597556b37993ea38f4f6641c7813dfd9fa5d
donglyc <silencesoon@vip.qq.com>
2021-05-18 11:37:00 +0800

    fix: update

---
+++ src/service/authing.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { AuthenticationClient } from 'authing-js-sdk';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class AuthingSDK {
+	authing: AuthenticationClient;
+	constructor() {
+		this.authing = new AuthenticationClient({
+			appId: '60a25996bf994ab22e1ac5a4',
+			appHost: 'https://spa-demo-angular.authing.cn',
+			redirectUri: 'http://localhost:4200/user-info',
+			tokenEndPointAuthMethod: 'none'
+		});
+	}
+}
--- src/views/user-info/userinfo.component.ts
+++ src/views/user-info/userinfo.component.ts
@@ -1,7 +1,64 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { AuthingSDK } from '../../service/authing.service';
 @Component({
 	selector: 'app-user-info',
 	templateUrl: './userinfo.component.html'
 })
-export class UserInfoComponent {}
+export class UserInfoComponent implements OnInit {
+	loading: boolean;
+	userInfo: string;
+	constructor(private AuthingSDK: AuthingSDK, private activateInfo: ActivatedRoute, private router: Router) {
+		this.userInfo = '';
+		this.loading = false;
+	}
+	ngOnInit() {
+		this.activateInfo.queryParams.subscribe((params: Params) => {
+			if (Object.keys(params).length > 0) {
+				this.handleCallback(params);
+			}
+		});
+	}
+	async handleCallback(params: Params) {
+		this.setLoading(true);
+		try {
+			const accessToken = localStorage.getItem('accessToken');
+			if (accessToken) {
+				this.setUserInfo();
+				this.setLoading(false);
+			} else {
+				const { authing } = this.AuthingSDK;
+				const { code } = params;
+				const codeChallenge = localStorage.getItem('codeChallenge');
+				const tokenSet = await authing.getAccessTokenByCode(code, { codeVerifier: codeChallenge as string });
+				const { access_token, id_token } = tokenSet;
+				const userInfo = await authing.getUserInfoByAccessToken(tokenSet.access_token);
+				localStorage.setItem('accessToken', access_token);
+				localStorage.setItem('idToken', id_token);
+				localStorage.setItem('userInfo', JSON.stringify(userInfo));
+				this.setUserInfo();
+				this.setLoading(false);
+			}
+		} catch (error) {
+			console.log('error', JSON.stringify(error));
+			this.router.navigate(['/login']);
+		}
+	}
+	setUserInfo() {
+		const userInfo = localStorage.getItem('userInfo');
+		this.userInfo = userInfo ? JSON.stringify(userInfo) : 'none';
+	}
+	setLoading(status: boolean) {
+		this.loading = status;
+	}
+	handleLogout() {
+		const { authing } = this.AuthingSDK;
+		const idToken = localStorage.getItem('idToken');
+		localStorage.clear();
+		if (idToken) {
+			const url = authing.buildLogoutUrl({ expert: true, redirectUri: 'http://localhost:4200', idToken });
+			console.log(url);
+			// window.location.href = url;
+		}
+	}
+}
