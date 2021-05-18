22d5bcb15680ebb655ca2030fda523511ae40192
donglyc <silencesoon@vip.qq.com>
2021-05-18 10:30:31 +0800

    fix: update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../views/login/login.component';
+import { UserInfoComponent } from '../views/user-info/userinfo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: LoginComponent
+	},
+	{
+		path: 'login',
+		component: LoginComponent
+	},
+	{
+		path: 'user-info',
+		component: UserInfoComponent
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
