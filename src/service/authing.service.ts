a5545b5f161138141a7bfe2c694bcec10c096d53
donglyc <silencesoon@vip.qq.com>
2021-05-20 16:23:42 +0800

    update

--- src/service/authing.service.ts
+++ src/service/authing.service.ts
@@ -7,10 +7,9 @@ export class AuthingSDK {
 	authing: AuthenticationClient;
 	constructor() {
 		this.authing = new AuthenticationClient({
-			appId: '60a25996bf994ab22e1ac5a4',
-			appHost: 'https://spa-demo-angular.authing.cn',
-			redirectUri: 'http://localhost:4200/user-info',
-			tokenEndPointAuthMethod: 'none'
+			appId: 'APP_ID',
+			appHost: 'https://{你的域名}.authing.cn',
+			redirectUri: 'http://localhost:4000/callback'
 		});
 	}
 }
